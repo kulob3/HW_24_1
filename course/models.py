791469db650d03472b8eb75c49da91495707e67d
Alex Urapin <kulob1@mail.ru>
2024-11-11 10:49:28 +0400

    add 1

--- course/models.py
+++ course/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name="name", help_text="indicate course name"
+    )
+    preview = models.ImageField(
+        upload_to="courses/previews/", verbose_name="preview", blank=True, null=True
+    )
+    description = models.TextField(
+        verbose_name="description",
+        help_text="indicate course description",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "course"
+        verbose_name_plural = "courses"
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, verbose_name="name")
+    description = models.TextField(
+        verbose_name="description",
+        blank=True,
+        null=True,
+        help_text="indicate lesson description",
+    )
+    preview = models.ImageField(
+        upload_to="lessons/previews/", verbose_name="preview", blank=True, null=True
+    )
+    video = models.FileField(
+        upload_to="lessons/videos/", verbose_name="video", blank=True, null=True
+    )
+    course = models.ForeignKey(
+        Course, on_delete=models.SET_NULL, related_name="lessons", verbose_name="course",
+        blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "lesson"
+        verbose_name_plural = "lessons"
+
+    def __str__(self):
+        return self.name
---
+++ course/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from course.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView
+from course.apps import CourseConfig
+
+app_name = CourseConfig.name
+
+router = SimpleRouter()
+router.register('', CourseViewSet)
+
+urlpatterns = [
+    path('lessons/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('lessons/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-retrieve'),
+    path('lessons/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lessons/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+]
+urlpatterns += router.urls
